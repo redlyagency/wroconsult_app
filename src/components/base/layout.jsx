1ed6ce8ae046cd33eb0e21a7f506bddb701dd6dd
rackowsky <bartek@wroconsult.pl>
2021-11-06 00:12:54 +0100

    preloader added

--- src/components/base/layout.jsx
+++ src/components/base/layout.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
 import { Helmet } from "react-helmet"
 import CookieConsent from 'react-cookie-consent'
 import "@fontsource/poppins"
@@ -12,7 +13,100 @@ import { GlobalStyle } from "../../utils/theme/global.theme"
 
 import { CookieConsentWrapper } from "../../styles/index.style"
 
-const Layout = (props) => {
+const Preloader = styled.div`
+    background-color: #BF1E2D;
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100vh;
+    width: 100vw;
+    z-index: 99999999999999999;
+    transition: 1s;
+    animation-name: ease-out1;
+    animation-duration: 1.5s;
+
+    @keyframes ease-out1 {
+        0% {
+            opacity: 1;
+        }
+        50% {
+            opacity: 1;
+        }
+        80% {
+            opacity: 0;
+        }
+    }
+`
+const PreloaderSymbol = styled.div`
+    position: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #BF1E2D;
+
+    &:before,
+    &:after {
+        content: "";
+        display: block;
+        position: absolute;
+        box-sizing: border-box;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        margin: auto;
+        height: 70px;
+        width: 70px;
+        border: 2px white solid;
+        border-radius: 50%;
+        opacity: 1;
+        animation: loader-1-ripple 3s cubic-bezier(0.075, 0.82, 0.165, 1) infinite;
+    }
+
+    &:after {
+        animation: loader-1-ripple2 3s cubic-bezier(0.075, 0.82, 0.165, 1) 0.5s
+            infinite;
+    }
+
+    @keyframes loader-1-ripple {
+        0% {
+            transform: translate3d(0, 0, 0) scale(0);
+            opacity: 1;
+        }
+        20% {
+            opacity: 1;
+        }
+        100% {
+            transform: translate3d(0, 0, 0) scale(1.5);
+            opacity: 0;
+        }
+    }
+
+    @keyframes loader-1-ripple2 {
+        0% {
+            transform: translate3d(0, 0, 0) scale(0);
+            opacity: 1;
+        }
+        20% {
+            opacity: 1;
+        }
+        100% {
+            transform: translate3d(0, 0, 0) scale(1);
+            opacity: 0;
+        }
+    }
+`
+
+const Layout = (props, { children }) => {
+    const [loader, setLoader]=useState(true);
+
+    useEffect(()=>{
+        setTimeout(()=> {
+        setLoader(false)
+        }, 1000)
+    }, [])
+
     return (
         <>
             <div className="container">
@@ -38,6 +132,11 @@ const Layout = (props) => {
                 </Helmet>
                 <Header />
                     {props.children}
+                    {loader ?
+                        <Preloader>
+                            <PreloaderSymbol />
+                        </Preloader>
+                    :  children}
                 <CookieConsentWrapper
                     style={{
                         position: 'fixed'
