1dc4d7e0cce0b7d32047d95b4065c411e06de781
rackowsky <bartek@wroconsult.pl>
2022-01-13 11:42:18 +0100

    formik, yup configuration - validation

--- src/components/form/contactFormulage.jsx
+++ src/components/form/contactFormulage.jsx
@@ -1,192 +1,98 @@
 import React from "react"
-import styled from "styled-components"
-import { Formik } from "formik"
+import { useFormik } from "formik"
+import * as Yup from 'yup'
 import {
     ContactFormulageWrapper,
     H2,
-
+    Form,
+    FormFlexWrapper,
+    LeftCol,
+    RightCol,
+    Field,
+    Input,
+    Label,
+    Textarea,
+    LabelTextarea,
+    Button,
+    ErrorAlert,
 } from "./contactFormulage.style"
 
-const Form = styled.form`
-    margin-top: 50px;
-
-`
-const Field = styled.div`
-    position: relative;
-`
-const Input = styled.input`
-    background-color: transparent;
-    touch-action: manipulation;
-    transform-origin: left bottom;
-    height: 50px;
-    width: 100%;
-    border-bottom: 2px solid #707070;
-    padding-left: 10px;
-    font-size: 16px;
-    font-style: italic;
-    color: #76777A;
-    font-weight: 600;
-
-    :focus {
-        border-bottom: 2px solid #BF1E2D;
-    }
-
-    :placeholder-shown + label {
-        position: absolute;
-        left: 10px;
-        top: 50%;
-        transform: translateY(-50%);
-        color: #BF1E2D;
-        font-weight: 600;
-        font-style: italic;
-        font-size: 14px;
-    }
-
-    :not(:placeholder-shown) + label, :focus + label {
-        position: absolute;
-        left: 0;
-        top: -5px;
-        color: #BF1E2D;
-        font-weight: 600;
-        font-style: italic;
-        font-size: 14px;
-        pointer-events: none;
-        transition: 0.2s;
-        touch-action: manipulation;
-        transform-origin: left bottom;
-    }
-`
-const Label = styled.label`
-    position: absolute;
-    left: 0;
-    top: -15px;
-    color: #BF1E2D;
-    font-weight: 600;
-    font-style: italic;
-    font-size: 14px;
-    pointer-events: none;
-    transition: 0.2s;
-    touch-action: manipulation;
-    transform-origin: left bottom;
-`
-const LabelTextarea = styled.label`
-    position: absolute;
-    left: 10px;
-    top: 14px;
-    color: #BF1E2D;
-    font-weight: 600;
-    font-style: italic;
-    font-size: 14px;
-`
-const Textarea = styled.textarea`
-    background-color: transparent;
-    resize: none;
-    height: 200px;
-    width: 100%;
-    border-bottom: 2px solid #707070;
-    padding: 10px;
-    touch-action: manipulation;
-    transform-origin: left bottom;
-    font-size: 16px;
-    font-style: italic;
-    color: #76777A;
-    font-weight: 600;
-
-    :not(:placeholder-shown) + label, :focus + label {
-        position: absolute;
-        left: 0;
-        top: -15px;
-        color: #BF1E2D;
-        font-weight: 600;
-        font-style: italic;
-        font-size: 14px;
-        pointer-events: none;
-        transition: 0.2s;
-        touch-action: manipulation;
-        transform-origin: left bottom;
-    }
-`
-const LeftCol = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 200px;
-    width: 50%;
-`
-const RightCol = styled.div`
-    height: 200px;
-    width: 50%;
-`
-const Button = styled.button`
-    background-color: #BF1E2D;
-    border: 2px solid #BF1E2D;
-    width: 100%;
-    height: 50px;
-    color: white;
-    font-weight: 500;
-    border-radius: 10px;
-    margin-top: 25px;
-
-    :hover {
-        transition: 0.2s;
-        background-color: transparent;
-        color: #BF1E2D;
-    }
-`
-const FormFlexWrapper = styled.div`
-    display: flex;
-    gap: 10px;
-`
-
 const ContactFormulage = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            name: '',
+            message: '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().email('Niepoprawny adres email').required('To pole jest wymagane'),
+            name: Yup.string().required('To pole jest wymagane'),
+            message: Yup.string().required('To pole jest wymagane'),
+        }),
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    })
     return(
         <ContactFormulageWrapper>
             <H2>Skontaktuj się z nami poprzez formularz kontaktowy</H2>
-            <Formik>
-                {({
-
-                }) => (
-                    <Form>
-                        <FormFlexWrapper>
-                            <LeftCol>
-                                {/* email field */}
-                                <Field>
-                                    <Input
-                                        type="email"
-                                        name="email"
-                                        id="email"
-                                        placeholder=" "
-                                    />
-                                    <Label for="email">Adres Email</Label>
-                                </Field>
-                                {/* name field */}
-                                <Field>
-                                    <Input
-                                        type="text"
-                                        name="name"
-                                        id="name"
-                                        placeholder=" "
-                                    />
-                                    <Label for="name">Nazwa Podmiotu</Label>
-                                </Field>
-                            </LeftCol>
-                            {/* message field */}
-                            <RightCol>
-                                <Field>
-                                    <Textarea
-                                        name="message"
-                                        id="message"
-                                        placeholder=" "
-                                    />
-                                    <LabelTextarea for="message">Treść wiadomości</LabelTextarea>
-                                </Field>
-                            </RightCol>
-                        </FormFlexWrapper>
-                        {/* submit button */}
-                        <Button>Wyślij</Button>
-                    </Form>
-                )}
-            </Formik>
+            <Form onSubmit={formik.handleSubmit}>
+                <FormFlexWrapper>
+                    <LeftCol>
+                        {/* email field */}
+                        <Field>
+                            <Input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder=" "
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.email}
+                            />
+                            <Label for="email">Adres Email</Label>
+                        </Field>
+                        {formik.touched.email && formik.errors.email ? (
+                            <ErrorAlert>{formik.errors.email}</ErrorAlert>
+                        ) : null}
+                        {/* name field */}
+                        <Field>
+                            <Input
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder=" "
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.name}
+                            />
+                            <Label for="name">Nazwa Podmiotu</Label>
+                        </Field>
+                        {formik.touched.name && formik.errors.name ? (
+                            <ErrorAlert>{formik.errors.name}</ErrorAlert>
+                        ) : null}
+                    </LeftCol>
+                    {/* message field */}
+                    <RightCol>
+                        <Field>
+                            <Textarea
+                                name="message"
+                                id="message"
+                                placeholder=" "
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.message}
+                            ></Textarea>
+                            <LabelTextarea for="message">Treść wiadomości</LabelTextarea>
+                        </Field>
+                        {formik.touched.message && formik.errors.message ? (
+                            <ErrorAlert>{formik.errors.message}</ErrorAlert>
+                        ) : null}
+                    </RightCol>
+                </FormFlexWrapper>
+                {/* submit button */}
+                <Button>Wyślij</Button>
+            </Form>
         </ContactFormulageWrapper>
     )
 }
--- src/components/form/contactFormulage.style.js
+++ src/components/form/contactFormulage.style.js
@@ -15,3 +15,141 @@ export const H2 = styled.h2`
         font-size: 25px;
     }
 `
+export const Form = styled.form`
+    margin-top: 50px;
+
+`
+export const Field = styled.div`
+    position: relative;
+`
+export const Input = styled.input`
+    background-color: transparent;
+    touch-action: manipulation;
+    transform-origin: left bottom;
+    height: 50px;
+    width: 100%;
+    border-bottom: 2px solid #707070;
+    padding-left: 10px;
+    font-size: 16px;
+    font-style: italic;
+    color: #76777A;
+    font-weight: 600;
+
+    :focus {
+        border-bottom: 2px solid #BF1E2D;
+    }
+
+    :placeholder-shown + label {
+        position: absolute;
+        left: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        color: #BF1E2D;
+        font-weight: 600;
+        font-style: italic;
+        font-size: 14px;
+    }
+
+    :not(:placeholder-shown) + label, :focus + label {
+        position: absolute;
+        left: 0;
+        top: -5px;
+        color: #BF1E2D;
+        font-weight: 600;
+        font-style: italic;
+        font-size: 14px;
+        pointer-events: none;
+        transition: 0.2s;
+        touch-action: manipulation;
+        transform-origin: left bottom;
+    }
+`
+export const Label = styled.label`
+    position: absolute;
+    left: 0;
+    top: -15px;
+    color: #BF1E2D;
+    font-weight: 600;
+    font-style: italic;
+    font-size: 14px;
+    pointer-events: none;
+    transition: 0.2s;
+    touch-action: manipulation;
+    transform-origin: left bottom;
+`
+export const LabelTextarea = styled.label`
+    position: absolute;
+    left: 10px;
+    top: 14px;
+    color: #BF1E2D;
+    font-weight: 600;
+    font-style: italic;
+    font-size: 14px;
+`
+export const Textarea = styled.textarea`
+    background-color: transparent;
+    resize: none;
+    height: 200px;
+    width: 100%;
+    border-bottom: 2px solid #707070;
+    padding: 10px;
+    touch-action: manipulation;
+    transform-origin: left bottom;
+    font-size: 16px;
+    font-style: italic;
+    color: #76777A;
+    font-weight: 600;
+
+    :not(:placeholder-shown) + label, :focus + label {
+        position: absolute;
+        left: 0;
+        top: -15px;
+        color: #BF1E2D;
+        font-weight: 600;
+        font-style: italic;
+        font-size: 14px;
+        pointer-events: none;
+        transition: 0.2s;
+        touch-action: manipulation;
+        transform-origin: left bottom;
+    }
+`
+export const LeftCol = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 200px;
+    width: 50%;
+`
+export const RightCol = styled.div`
+    height: 200px;
+    width: 50%;
+`
+export const Button = styled.button`
+    background-color: #BF1E2D;
+    border: 2px solid #BF1E2D;
+    width: 100%;
+    height: 50px;
+    color: white;
+    font-weight: 500;
+    border-radius: 10px;
+    margin-top: 30px;
+
+    :hover {
+        transition: 0.2s;
+        background-color: transparent;
+        color: #BF1E2D;
+    }
+`
+export const FormFlexWrapper = styled.div`
+    display: flex;
+    gap: 10px;
+`
+export const ErrorAlert = styled.p`
+    color: #BF1E2D;
+    font-weight: 600;
+    font-style: italic;
+    font-size: 12px;
+    margin: -35px 0 0 0;
+    padding: 0;
+`
